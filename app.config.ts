456930b6d55b709509edf8665282944d41fb53b6
Jorge Alejandro Casares Delgado <jorgealejandrocasaresdelgado@gmail.com>
2026-01-15 01:49:15 -0500

    versionando

--- app.config.ts
+++ app.config.ts
@@ -25,7 +25,7 @@ const config: ExpoConfig = {
   name: env.appName,
   slug: "foteria-tracking-20",
   
-  version: "1.4.0",
+  version: "1.5.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
